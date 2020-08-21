a94bc1ea4c8b5ec4b6e0066d625800d0c7a6f730
Tony Bellavia <at.bellavia@gmail.com>
2020-08-22 00:06:52 +0200

    Add : 	- Docker image build 	- Progress bar 	- Preserve session

--- setup.sh
+++ setup.sh
@@ -19,6 +19,7 @@ ORANGE='\033[0;33m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
+PBAR_SIZE=50
 
 
 
@@ -32,6 +33,55 @@ HEADER=$(cat <<HEADER_S
 HEADER_S
 )
 
+ProgressBar()
+{
+	#	Provide a progress bar.
+	#
+	#	ProgressBar [--help], [done,  target,  size,  advance,  left,  begin,  ending,  label]
+	#	 Parameters
+	#	 	:done:		the current progress depending on target
+	#	 	:target:	the target number to achieve
+	#	 	:size:		the size of the ProgressBar
+	#	 	:advance:	the character representing the advanced into the progress bar
+	#	 	:left:		the character representing the left space into the progress bar
+	#	 	:begin:		the trailing character representing progress bar begin delimiter
+	#	 	:ending:	the trailing character representing progress bar end delimiter
+	#	 	:label:		the progress bar label
+	#
+
+	if [ "$#" != 8 ]
+	then
+		printf "$_help\n"
+		exit 1
+	fi
+
+	_progress=$(( ${1} * 100 / ${2} * 100 / 100 ))
+	_done=$(( ${_progress} * ${3} / 10 / 10 ))
+	_left=$(( ${3} - $_done ))
+
+	_fill=$(printf "%${_done}s")
+	_empty=$(printf "%${_left}s")
+
+	printf "\r${8} : ${6}${_fill// /${4}}${_empty// /${5}}${7} ${_progress}%%"
+}
+
+BasicProgressBar()
+{
+	#	Provide a basic progress bar.
+	#
+	#	ProgressBar [--help], [done,  target,  size, label]
+	#	 Parameters
+	#	 	:done:		the current progress depending on target
+	#	 	:target:	the target number to achieve
+	#	 	:size:		the size of the ProgressBar
+	#	 	:label:		the progress bar label
+	#
+	if [ "$#" != 4 ]
+	then
+		exit 1
+	fi
+	ProgressBar "${1}" "${2}" "${3}" "#" " " "[" "]" "${4}"
+}
 
 status_msg()
 {
@@ -51,12 +101,19 @@ status_msg()
 	fi
 }
 
-category()
+print_category()
 {
 	if [ "$#" = "1" ]
 	then
+		echo ""
+		echo ""
 		printf ">>> $YELLOW$1$NC \n"
 	fi
+}
+
+print_header()
+{
+	echo "$HEADER"
 }
 
 select_driver()
@@ -91,12 +148,18 @@ minikube_setup()
 		exit 1
 	fi
 
-	if minikube delete &> /dev/null
+	if [[ "${_preserve_session}" == 1 ]]
 	then
-		status_msg success "Removing old minikube clusters"
+		status_msg success "Preserve session : ${BLUE}on${NC}"
 	else
-		status_msg danger "Removing old minikube clusters failed"
-		exit 1
+		status_msg warning "Preserve session : ${RED}off${NC}"
+		if minikube delete &> /dev/null
+		then
+			status_msg success "Removing old minikube clusters"
+		else
+			status_msg danger "Removing old minikube clusters failed"
+			exit 1
+		fi
 	fi
 
 	status_msg success "Starting minikube ... be patient"
@@ -109,18 +172,71 @@ minikube_setup()
 	fi
 }
 
+print_docker_images()
+{
+	folders="${1}"
+
+	printf "%-20s %-20s\n" "IMAGE" "STATUS"
+	for index in ${!folders[*]}
+	do
+		printf "%-20s ${GREEN}%-20s${NC}\n" "$(basename ${folders[${index}]})" "created"
+		sleep 0.2
+	done
+}
+
+setup_images()
+{
+	folders=()
+
+	eval $(minikube -p minikube docker-env)
+
+	for folder in $(ls -d ./srcs/images/**)
+	do
+		folders+=( "${folder}" )
+	done
+
+	size="${#folders[@]}"
+
+	for index in ${!folders[*]} ; do 
+		image="${folders[${index}]}"
+		image_name="$(basename ${image})"
+
+		if docker build -t "${image_name}" "${image}" &> /dev/null
+		then
+			BasicProgressBar "$(( index + 1 ))" "$size" "${PBAR_SIZE}" "Building images" 
+		else
+			echo ""
+			echo ""
+			status_msg danger "Building ${image_name} failed"
+			exit 1
+		fi
+	done
+
+	echo ""
+
+	print_docker_images "$folders"
+}
+
 main()
 {
 	drivers=("virtualbox" "docker")
 	default=${drivers[0]}
 	driver=$default
+	_preserve_session=0
 
-	echo "$HEADER"
-	echo ""
-	echo ""
-	category "Minikube"
+	if [ "$1" == "--preserve-session" ]
+	then
+		_preserve_session=1
+	fi
+
+	print_header
+
+	print_category "Minikube"
 	select_driver
 	minikube_setup
+
+	print_category "Docker"
+	setup_images
 }
 
-main
+main $@
