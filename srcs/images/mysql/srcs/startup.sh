5915ca292164a8535ddc01509b0305a6a9367ec0
Tony Bellavia <at.bellavia@gmail.com>
2020-09-04 18:42:04 +0200

    Add Wordpress sql file for adding users

--- srcs/images/mysql/srcs/startup.sh
+++ srcs/images/mysql/srcs/startup.sh
@@ -1,5 +1,6 @@
 service mariadb restart &> /dev/null
 mysql < /tmp/create_tables.sql
 mysql < /tmp/create_users.sql
+mysql -u root -p wordpress --password='root' < /tmp/wordpress.sql
 
 tail -F /dev/null
---
+++ srcs/manifests/configmaps/wordpress/wp-config.php
@@ -0,0 +1,36 @@
+<?php
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress' );
+
+/** MySQL database username */
+define( 'DB_USER', 'wp' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'root' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'mysql' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+define( 'AUTH_KEY',         '' );
+define( 'SECURE_AUTH_KEY',  '' );
+define( 'LOGGED_IN_KEY',    '' );
+define( 'NONCE_KEY',        '' );
+define( 'AUTH_SALT',        '' );
+define( 'SECURE_AUTH_SALT', '' );
+define( 'LOGGED_IN_SALT',   '' );
+define( 'NONCE_SALT',       '' );
+
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+if ( ! defined( 'ABSPATH' ) ) {
+        define( 'ABSPATH', __DIR__ . '/' );
+}
+require_once ABSPATH . 'wp-settings.php';
