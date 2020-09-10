d699f1f028faf8d8a41cb9bb87eeac57ae145965
Tony Bellavia <bbellavi@student.42.fr>
2020-09-10 20:06:09 +0200

    Modify ftps config, it now works

--- srcs/images/ftps/srcs/startup.sh
+++ srcs/images/ftps/srcs/startup.sh
@@ -1,3 +1,6 @@
-rc-service vsftpd start
+echo -e "Admin-1234\nAdmin-1234" | adduser -h /ftp/admin admin
+# rc-service vsftpd start
+chown admin:admin /ftp/admin
+exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21005 -opasv_address=127.0.0.1 /etc/vsftpd/vsftpd.conf &
 
 tail -F /dev/null
