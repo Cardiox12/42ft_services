6b5eb9399a98fa655170e72676809b27fa6ba51c
Tony Bellavia <bbellavi@student.42.fr>
2020-09-14 14:27:47 +0200

    Add opasv_addr to listen on bind Vbox network interface

--- srcs/images/ftps/srcs/startup.sh
+++ srcs/images/ftps/srcs/startup.sh
@@ -1,5 +1,5 @@
 echo -e "Admin-1234\nAdmin-1234" | adduser -h /ftp/admin admin
 chown admin:admin /ftp/admin
-exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21005 etc/vsftpd/vsftpd.conf &
+exec /usr/sbin/vsftpd -opasv_min_port=21000 -opasv_max_port=21005 -opasv_address=192.168.99.230 etc/vsftpd/vsftpd.conf &
 
 tail -F /dev/null
